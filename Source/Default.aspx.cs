9a69c025b305626139b3956b91ac6a6888c14f9d
thecelestialjewel <christinemjordan6@gmail.com>
2021-02-13 00:15:11 -0800

    Server side web method created. Ran into some difficulties. Web methods must always be static. Thought there was issues with authorization.

--- Source/Default.aspx.cs
+++ Source/Default.aspx.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿//Kelcee Chorba and Christine Jordan 2.12.2021 CIS235 W6 Team Project
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,6 +18,35 @@ namespace Interactive_Learning
 
         }
 
+        [WebMethod]
+        public static string GetJumbotronText(string tabText)
+        {
+            JumbotronText jumbotronText = new JumbotronText();
+            switch (tabText)
+            {
+                case "Learn Numbers":
+                    jumbotronText.LearningTitle = "Learning Numbers is Fun!";
+                    jumbotronText.Description = "Count the ballons and click to see the number!";
+                    break;
+                case "Learn Colors":
+                    jumbotronText.LearningTitle = "Learning Colors is Fun!";
+                    jumbotronText.Description = "Guess the color and click to see the color name!";
+                    break;
 
+                case "Learn Animals":
+                    jumbotronText.LearningTitle = "Learning Animals is Fun!";
+                    jumbotronText.Description = "Guess the animal and click to see the name of the animal!";
+                    break;
+
+                default:
+                    throw new Exception("Unknown tab.");
+            }
+            return JsonConvert.SerializeObject(jumbotronText);
+        }
+        public class JumbotronText
+        {
+            public string LearningTitle { get; set; }
+            public string Description { get; set; }
+        }
     }
 }
